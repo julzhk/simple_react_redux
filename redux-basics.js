27c3410c8950f8791805edbdf4a06eb763dffedc
julian <julian.harley@gmail.com>
2020-04-12 21:55:54 +0100

    add simple redux subscription - 257

--- redux-basics.js
+++ redux-basics.js
@@ -8,14 +8,14 @@ const initialState = {
 
 //REDUCER
 const rootReducer = (state = initialState, action) => {
-    if(action.type==='INC_COUNTER'){
+    if (action.type === 'INC_COUNTER') {
         // must update state IMMUTABLY!
         return {
             ...state,
             counter: state.counter + 1
         }
     }
-    if(action.type==='ADD_COUNTER'){
+    if (action.type === 'ADD_COUNTER') {
         // must update state IMMUTABLY!
         return {
             ...state,
@@ -31,11 +31,15 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 //1
 
+// SUBSCRIPTION
+store.subscribe(() => {
+    console.log('[SUBSCRIPTION', store.getState())
+})
+
+
 // ACTION DISPATCHER
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
 
 console.log(store.getState());
 // 11
-
-// SUBSCRIPTION
