d52584da0d3d2d472806a3c5691d861c3a7170d5
julian <julian.harley@gmail.com>
2020-04-12 22:39:20 +0100

    update dependencies; initialize redux - 259

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import {connect} from "react-redux";
+
+
 
 class Counter extends Component {
     state = {
@@ -28,7 +31,7 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
+                <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
@@ -38,4 +41,11 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+//specify what state vars & actions that are needed
+// property name here is ctr
+const mapStateToProps = (state) => {
+    return{
+        ctr: state.counter
+    }
+}
+export default connect()(Counter);
