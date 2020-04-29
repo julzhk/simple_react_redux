8d892b5a8d4dab81559cfed268cdaccbb2abb812
julian <julian.harley@gmail.com>
2020-04-29 09:35:46 +0100

    add middleware logger - 284

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {combineReducers, createStore} from "redux";
+import {combineReducers, createStore, applyMiddleware} from "redux";
 import counterReducer from "./store/reducers/counter";
 import resultReducer from "./store/reducers/result";
 import {Provider} from "react-redux";
@@ -15,7 +15,22 @@ const rootReducer = combineReducers(
         resultState: resultReducer
     }
 )
-const store = createStore(rootReducer);
+const logger = store => {
+    //returns a function
+    return next => {
+        //returns a function again!
+        return action => {
+            console.log('[middleware] dispatching ',action)
+            console.log('[middleware] current state ',store.getState())
+            const result = next(action)
+            console.log('[middleware] next state ',store.getState())
+            return result
+        }
+    }
+}
+
+// add 'enhancer(s)'
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 
 ReactDOM.render(<Provider store={store}><App/></Provider>,
